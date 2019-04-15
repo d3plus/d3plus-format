a4c3faabdb1ec9bb8e02be0b63a4831bd80392e5
cnavarreteliz <cnavarreteliz@gmail.com>
2019-04-15 18:10:21 -0400

    enables custom formatters according the locale selected

--- src/abbreviate.js
+++ src/abbreviate.js
@@ -1,25 +1,57 @@
 import {format} from "d3-format";
+import defaultLocale from "./locale.json";
+
+const round = (x, n) => 
+  parseFloat(Math.round(x * Math.pow(10, n)) / Math.pow(10, n)).toFixed(n);
+
+/** */
+function formatSuffix(value, precision, suffixes) {
+  let i = 0;
+  if (value) {
+    if (value < 0) value *= -1;
+    i = 1 + Math.floor(1e-12 + Math.log(value) / Math.LN10);
+    i = Math.max(-24, Math.min(24, Math.floor((i - 1) / 3) * 3));
+  }
+  const d = suffixes[8 + i / 3];
+  
+  return {
+    number: round(d.scale(value), precision),
+    symbol: d.symbol
+  };
+}
+
+/** */
+function parseSuffixes(d, i) {
+  const k = Math.pow(10, Math.abs(8 - i) * 3);
+  return {
+    scale: i > 8 ? d => d / k : d => d * k,
+    symbol: d
+  };
+}
+
 
 /**
     @function formatAbbreviate
     @desc Formats a number to an appropriate number of decimal places and rounding, adding suffixes if applicable (ie. `1200000` to `"1.2M"`).
-    @param {Number} n The number to be formatted.
+    @param {Number|String} n The number to be formatted.
+    @param {String} locale The locale to be formatted.
     @returns {String}
 */
-export default function(n) {
-  if (typeof n !== "number") return "N/A";
+export default function(n, locale = "et") {
+  // isFinite
+  if (typeof n !== "number") n *= 1;
+  const specifier = "s";
+  const suffixes = defaultLocale[locale].suffixes.map(parseSuffixes);
+
   const length = n.toString().split(".")[0].replace("-", "").length;
   let val;
   if (n === 0) val = "0";
   else if (length >= 3) {
-    const f = format(".3s")(n)
-      .replace("G", "B")
-      .replace("T", "t")
-      .replace("P", "q")
-      .replace("E", "Q");
-    const num = f.slice(0, -1);
-    const char = f.slice(f.length - 1);
-    val = `${parseFloat(num)}${char}`;
+    const f = formatSuffix(n, 2, suffixes);
+    const num = f.number;
+    const char = f.symbol;
+    const prefix = specifier === "c" ? "$" : "";
+    val = `${prefix}${parseFloat(num)}${char}`;
   }
   else if (length === 3) val = format(",f")(n);
   else if (n < 1 && n > -1) val = format(".2g")(n);
