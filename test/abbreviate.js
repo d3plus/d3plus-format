6bce9cde12315971ae32994ee1d26ad38e4243ff
cnavarreteliz <cnavarreteliz@gmail.com>
2019-04-16 14:33:29 -0400

    creates test for estonian and spanish format

--- test/abbreviate.js
+++ test/abbreviate.js
@@ -8,8 +8,8 @@ test("abbreviate", assert => {
   assert.equal("1.23t", abbreviate(1234567890000), "trillion");
   assert.equal("1.23B", abbreviate(1234567890), "billion");
   assert.equal("1.23M", abbreviate(1234567), "million");
-  assert.equal("123.46k",  abbreviate(123456), "hundred thousand");
-  assert.equal("12.35k", abbreviate(12345), "ten thousand");
+  assert.equal("123k",  abbreviate(123456), "hundred thousand");
+  assert.equal("12.3k", abbreviate(12345), "ten thousand");
   assert.equal("1.23k", abbreviate(1234), "thousand");
   assert.equal("123",   abbreviate(123), "hundred");
   assert.equal("12",    abbreviate(12), "ten");
@@ -19,6 +19,9 @@ test("abbreviate", assert => {
 
   assert.equal("1B",    abbreviate(1000000009), "large - removes trailing zeros and period");
   assert.equal("0.1",   abbreviate(0.1), "small - removes trailing zeros");
+
+  assert.equal("10 mln", abbreviate(10000000, "et-EE"), "estonian locale");
+  assert.equal("1mm",    abbreviate(1000000, "es-ES"), "spanish locale");
 
 });
 
